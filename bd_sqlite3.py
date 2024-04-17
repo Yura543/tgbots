b8962b40900cca16b32e954bdef25bc61c17f498
Yura543 <91012944+Yura543@users.noreply.github.com>
2024-04-18 00:21:26 +0300

    Add files via upload

---
+++ bd_sqlite3.py
@@ -0,0 +1,54 @@
+import telebot
+import sqlite3
+
+bot = telebot.TeleBot('6994914411:AAFGtlCtpzs8_Ch1QS3xGlV25NJzDcSPDe8')
+name = None
+@bot.message_handler(commands=['start'])
+def start(message):
+    conn = sqlite3.connect('yura.sql')
+    cur = conn.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS users(id int auto_increment primary key, name varchar(50), pass varchar(50))')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    bot.send_message(message.chat.id, 'Привет, братишка, сейчас тебя зарегаю! Введи имя')
+    bot.register_next_step_handler(message, user_name)
+
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, 'Введи пароль')
+    bot.register_next_step_handler(message, user_pass)
+
+def user_pass(message):
+    password = message.text.strip()
+    conn = sqlite3.connect('yura.sql')
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO users (name, pass) VALUES ('%s','%s')" % (name, password))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton('Список пользователей', callback_data='users'))
+    bot.send_message(message.chat.id, 'Готова! Зареган', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    conn = sqlite3.connect('yura.sql')
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+
+    info = ''
+    for el in users:
+        info += f'Имя: {el[1]}, Пароль: {el[2]}\n'
+    cur.close()
+    conn.close()
+
+    bot.send_message(call.message.chat.id, info)
+bot.polling(none_stop=True)
